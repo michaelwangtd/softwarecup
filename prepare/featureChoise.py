bdad019e3d4268fbd10b422bdcdedfd03129babc
CaptWang <michaelwangtd@gmail.com>
2017-05-19 00:03:35 +0800

    final rolling preidt

--- prepare/featureChoise.py
+++ prepare/featureChoise.py
@@ -15,13 +15,14 @@ from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
 
 if __name__ == '__main__':
     # inputFileName = 'car_sales.csv'
-    inputFileName = 'O1005-O1005_model_trial.csv'
+    # inputFileName = 'O1005-O1005_model_trial.csv'
+    inputFileName = 'O1002-PAX_model_trial.csv'
     inputFilePath = inout.getDataModelPipelinePath(inputFileName)
     out1FileName = 'O1005_diff_10.csv'
     out1FilePath = inout.getDataModelPipelinePath(out1FileName)
-    out2FileName = 'lags_features_origin.csv'
+    out2FileName = 'lags_features_origin_O1002.csv'
     out2FilePath = inout.getDataModelPipelinePath(out2FileName)
-    out3FileName = 'lags_features_clean.csv'
+    out3FileName = 'lags_features_clean_O1002.csv'
     out3FilePath = inout.getDataModelPipelinePath(out3FileName)
 
     diff_window = 10
@@ -31,6 +32,7 @@ if __name__ == '__main__':
     data = pd.read_csv(inputFilePath,header=None)
     ts = data[4]
     ts.index = data[0]
+    # ts_log = ts
     ts_log = np.log(ts)
 
     ##
@@ -49,17 +51,17 @@ if __name__ == '__main__':
     # plt.show()
 
     ## lags
-    # print type(ts_log.values)
-    # # print ts_log.values
-    # dataframe = DataFrame()
-    # for i in range(diff_window,0,-1):
-    #     dataframe['t-'+str(i)] = ts_log.shift(i)
-    # dataframe['t'] = ts_log.values
-    # # print dataframe.head(10)
-    # dataframe.to_csv(out2FilePath,index=False)
-    # # dataframe = dataframe[10:]
-    # dataframe.dropna(inplace=True)
-    # dataframe.to_csv(out3FilePath,index=False)
+    print type(ts_log.values)
+    # print ts_log.values
+    dataframe = DataFrame()
+    for i in range(diff_window,0,-1):
+        dataframe['t-'+str(i)] = ts_log.shift(i)
+    dataframe['t'] = ts_log.values
+    # print dataframe.head(10)
+    dataframe.to_csv(out2FilePath,index=False)
+    # dataframe = dataframe[10:]
+    dataframe.dropna(inplace=True)
+    dataframe.to_csv(out3FilePath,index=False)
 
     ## 根据重要性选择滞后特征
     # dataframe = read_csv(out3FilePath,header=0)
@@ -80,22 +82,22 @@ if __name__ == '__main__':
     # plt.show()
 
     ##
-    dataframe = read_csv(out3FilePath, header=0)
-    array = dataframe.values
-    x = array[:, 0:-1]
-    y = array[:, -1]
-    rfe = RFE(RandomForestRegressor(n_estimators=500, random_state=1),lags_feature_num)
-    fit = rfe.fit(x, y)
-    print fit.support_
-    print 'selected features:'
-    names = dataframe.columns.values[0:-1]
-    for i in range(len(fit.support_)):
-        if fit.support_[i]:
-            print names[i]
-    names = dataframe.columns.values[0:-1]
-    ticks = [i for i in range(len(names))]
-    plt.bar(ticks,fit.ranking_)
-    plt.xticks(ticks,names)
-    plt.show()
+    # dataframe = read_csv(out3FilePath, header=0)
+    # array = dataframe.values
+    # x = array[:, 0:-1]
+    # y = array[:, -1]
+    # rfe = RFE(RandomForestRegressor(n_estimators=500, random_state=1),lags_feature_num)
+    # fit = rfe.fit(x, y)
+    # print fit.support_
+    # print 'selected features:'
+    # names = dataframe.columns.values[0:-1]
+    # for i in range(len(fit.support_)):
+    #     if fit.support_[i]:
+    #         print names[i]
+    # names = dataframe.columns.values[0:-1]
+    # ticks = [i for i in range(len(names))]
+    # plt.bar(ticks,fit.ranking_)
+    # plt.xticks(ticks,names)
+    # plt.show()
 
 
--- prepare/pipelineModelPrediction.py
+++ prepare/pipelineModelPrediction.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import pandas as pd
 from utils import inout
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn import svm
 import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import Lasso, Ridge
+from sklearn.model_selection import GridSearchCV
 
 """
     input file:proxy action记录文件
@@ -31,9 +35,10 @@ if __name__ == '__main__':
 
     # 这里的滞后特征列表可以由之前的过程得到
     lags_feature_list = ['t-6','t-3','t-2','t-1','t']
-    forecast_len = 30
+    forecast_len = 11
 
-    inFileName = 'lags_features_clean.csv'
+    # inFileName = 'lags_features_clean.csv'
+    inFileName = 'lags_features_clean_O1002.csv'
     inFilePath = inout.getDataModelPipelinePath(inFileName)
     data = pd.read_csv(inFilePath,header=0)
     data = data[lags_feature_list].values
@@ -41,22 +46,32 @@ if __name__ == '__main__':
     # 先把训练集/测试集数据分开
     train_data = data[:70]
     test_data = data[70:]
+    # print type(test_data[:,-1])
+    # exit(0)
     # print len(train_data)
     # print len(test_data)
     # 初始化预测查询表
     x_train = train_data[:,0:-1]
     y_train = train_data[:,-1]
-    print x_train
-    print type(x_train)
-    print y_train
-    print type(y_train)
-    exit(0)
+    # print x_train
+    # print type(x_train)
+    # print x_train.shape()
+    # print y_train
+    # print type(y_train)
+    # exit(0)
 
     base_num = len(y_train)
     predict_search_list = list(y_train)
 
-    lr = LogisticRegression()
+    alpha_can = np.logspace(-3, 2, 10)
+
+    # model = Ridge()
+    # lr = GridSearchCV(model, param_grid={'alpha': alpha_can}, cv=5)
+    lr = DecisionTreeRegressor(criterion='mse', max_depth=20)
+    # lr = LogisticRegression()
+    # lr = LinearRegression()
     # lr = svm.SVR(kernel='rbf',gamma=0.2,C=100)
+    # x_train = x_train.reshape(-1,1)
     lr.fit(x_train, y_train)
 
     y_hat_list = []
@@ -68,14 +83,27 @@ if __name__ == '__main__':
         predict_queue.append(predict_search_list[curr_index-3])
         predict_queue.append(predict_search_list[curr_index-2])
         predict_queue.append(predict_search_list[curr_index-1])
-        print predict_queue
+        predict_queue = np.array(predict_queue)
+        print i,predict_queue
+        print type(predict_queue)
+        # exit(0)
+
         y_hat = lr.predict(predict_queue)
-        y_hat_list.append(y_hat)
-        predict_search_list.append(y_hat)
-    print len(y_hat_list)
-    print y_hat_list
-
-
+        print 'result:',y_hat
+        y_hat_list.append(y_hat[0])
+        predict_search_list.append(y_hat[0])
+    print len(y_hat_list),y_hat_list
+    print len(test_data[:,-1]),test_data[:,-1]
+    # exit(0)
+    plt.figure()
+    ticks = [i for i in range(len(y_hat_list))]
+    y_hat = np.array(y_hat_list)
+    plt.subplot(1,1,1)
+    plt.plot(ticks,y_hat,'-b')
+    plt.plot(ticks,test_data[:,-1],'-r')
+    plt.show()
+    # pyplot.bar(ticks,fit.ranking_)
+    # pyplot.xticks(ticks,names)
 
     exit(0)
 
@@ -107,10 +135,10 @@ if __name__ == '__main__':
     x_test_time = x_time[60:]
     print x_test_time
 
-    pyplot.figure(facecolor='w')
-    pyplot.plot(x_time,y,'g-')
-    pyplot.plot(x_test_time,y_hat,'r-')
-    pyplot.show()
+    plt.figure(facecolor='w')
+    plt.plot(x_time,y,'g-')
+    plt.plot(x_test_time,y_hat,'r-')
+    plt.show()
 
 
 
